4a9892ae3f24141244295441a3c54d071ee3907b
togashi <togashi@airfront.co.jp>
2012-02-10 21:04:43 +0900

    UI整備 - res/layout/main.xml 変換&レベル調整UIレイアウト追加 - KKMushActivity.java UI表示を実装

--- src/jp/togashi/android/kkmush/KKMushActivity.java
+++ src/jp/togashi/android/kkmush/KKMushActivity.java
@@ -1,27 +1,127 @@
 package jp.togashi.android.kkmush;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class KKMushActivity extends Activity {
+public class KKMushActivity extends Activity implements OnSeekBarChangeListener {
     private static final String ACTION_INTERCEPT = "com.adamrocker.android.simeji.ACTION_INTERCEPT";
     private static final String REPLACE_KEY = "replace_key";
+    
+    private static final Pattern mGobi = Pattern.compile("([^a-zA-Z0-9 　。ッ]+)[ 　。!！\\?？]");
+    private static final Pattern mBunmatsu = Pattern.compile(".*[^a-z0-9 　。ッ]$");
+    
+    private String mInputStr = "";
+    private String mOutputStr = "";
+    
+    private String convert(final String src, final int level) {
+        String outStr = new String(src);
+        
+        if (level >= 1) {
+            outStr = outStr.replace("ん", "ン");
+        }
+        
+        if (level >= 2) {
+            outStr = outStr.replace("っ", "ッ");
+        }
+        
+        if (level >= 3) {
+            Matcher m = mGobi.matcher(outStr);
+            while (m.find()) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(outStr.substring(0, m.end(1))).append("ッ").append(outStr.substring(m.end(1)));
+                outStr = sb.toString();
+                m = mGobi.matcher(outStr);
+            }
+        }
+        
+        if (level >= 4) {
+            Matcher m = mBunmatsu.matcher(outStr);
+            if (m.matches()) {
+                outStr += "ッ";
+            }
+        }
+        
+        return outStr;
+    }
+    
+    private void update() {
+        int lv = 1;
+        SeekBar sb = (SeekBar)findViewById(R.id.seekBar1);
+        if (sb != null) {
+            lv = sb.getProgress() + 1;
+        }
+        
+        TextView tv = (TextView)findViewById(R.id.beforetext);
+        if (tv != null) {
+            tv.setText(mInputStr);
+        }
+        
+        mOutputStr = convert(mInputStr, lv);
+        
+        tv = (TextView)findViewById(R.id.aftertext);
+        if (tv != null) {
+            tv.setText(mOutputStr);
+        }
+    }
     
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent input = getIntent();
         String action = input.getAction();
         if (action != null && ACTION_INTERCEPT.equals(action)) {
-            String outStr = input.getStringExtra(REPLACE_KEY);
-            if (outStr != null) {
-                outStr = outStr.replace("ん", "ン");
-                outStr = outStr.replace("っ", "ッ");
-                Intent output = new Intent();
-                output.putExtra(REPLACE_KEY, outStr);
-                setResult(RESULT_OK, output);
+            mInputStr = input.getStringExtra(REPLACE_KEY);
+            if (TextUtils.isEmpty(mInputStr)) {
+                finish();
+                return;
             }
+            
+            setContentView(R.layout.main);
+            
+            SeekBar sb = (SeekBar)findViewById(R.id.seekBar1);
+            if (sb != null) {
+                sb.setOnSeekBarChangeListener(this);
+            }
+            update();
+            
+            Button btn = (Button)findViewById(R.id.okbutton);
+            if (btn != null) {
+                btn.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent output = new Intent();
+                        output.putExtra(REPLACE_KEY, mOutputStr);
+                        setResult(RESULT_OK, output);
+                        finish();
+                    }
+                });
+            }
+            
         }
-        finish();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+            boolean fromUser) {
+        update();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
     }
 }
