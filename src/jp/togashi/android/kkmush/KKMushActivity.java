7f54909c3909e3865e4440650761d0b1908ffe01
togashi <togashi@airfront.co.jp>
2012-02-13 22:59:24 +0900

    UI改良 -landscape用レイアウトを追加

--- src/jp/togashi/android/kkmush/KKMushActivity.java
+++ src/jp/togashi/android/kkmush/KKMushActivity.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.ClipboardManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -24,6 +25,11 @@ public class KKMushActivity extends Activity implements OnSeekBarChangeListener 
     
     private String mInputStr = "";
     private String mOutputStr = "";
+    
+    private static final int PROC_MODE_MUSHROOM = 0;
+    private static final int PROC_MODE_CLIPBOARD = 1;
+    
+    private int mMode = PROC_MODE_MUSHROOM;
     
     private String convert(final String src, final int level) {
         String outStr = new String(src);
@@ -82,33 +88,52 @@ public class KKMushActivity extends Activity implements OnSeekBarChangeListener 
         String action = input.getAction();
         if (action != null && ACTION_INTERCEPT.equals(action)) {
             mInputStr = input.getStringExtra(REPLACE_KEY);
-            if (TextUtils.isEmpty(mInputStr)) {
-                finish();
-                return;
-            }
-            
-            setContentView(R.layout.main);
-            
-            SeekBar sb = (SeekBar)findViewById(R.id.seekBar1);
-            if (sb != null) {
-                sb.setOnSeekBarChangeListener(this);
-            }
-            update();
-            
-            Button btn = (Button)findViewById(R.id.okbutton);
-            if (btn != null) {
-                btn.setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent output = new Intent();
-                        output.putExtra(REPLACE_KEY, mOutputStr);
-                        setResult(RESULT_OK, output);
-                        finish();
-                    }
-                });
-            }
-            
+            mMode = PROC_MODE_MUSHROOM;
         }
+        
+        if (TextUtils.isEmpty(mInputStr)) {
+            ClipboardManager cbm = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+            CharSequence cs = cbm.getText();
+            if (cs != null) {
+                mInputStr = cs.toString();
+                mMode = PROC_MODE_CLIPBOARD;
+            }
+        }
+        
+        if (TextUtils.isEmpty(mInputStr)) {
+            finish();
+            return;
+        }
+            
+        setContentView(R.layout.main);
+        
+        SeekBar sb = (SeekBar)findViewById(R.id.seekBar1);
+        if (sb != null) {
+            sb.setOnSeekBarChangeListener(this);
+        }
+        update();
+        
+        Button btn = (Button)findViewById(R.id.okbutton);
+        if (btn != null) {
+            btn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
+    }
+    
+    @Override
+    public void finish() {
+        if (!TextUtils.isEmpty(mOutputStr)) {
+            Intent output = new Intent();
+            output.putExtra(REPLACE_KEY, mOutputStr);
+            setResult(RESULT_OK, output);
+            ClipboardManager cbm = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+            cbm.setText(mOutputStr);
+        }
+        super.finish();
     }
 
     @Override
